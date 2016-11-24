97680a807fe5214ffd7696e70ef465bcfba468e1
harryawk <harry.kambey@gmail.com>
2016-11-24 07:46:54 +0700

    program utama NB_030

--- TubesDuaAi/src/tubesduaai/TubesDuaAi.java
+++ TubesDuaAi/src/tubesduaai/TubesDuaAi.java
@@ -22,16 +22,42 @@ public class TubesDuaAi {
         Scanner s = new Scanner(System.in);
         Instances dummy = null;
         NB_030 nb = new NB_030();
+        
         System.out.println("masukkan indeks kelas. indeks pertama = 0. file : iris.arff");
         Integer classidx = s.nextInt();
-//        nb.DataRead("C:\\Users\\harry\\Downloads\\mush.arff");
-        nb.DataRead("C:\\Program Files\\Weka-3-8\\data\\iris.arff", classidx);
+//        nb.load_model("C:\\Users\\harry\\GitHub\\NasiPadang\\TubesDuaAi\\coba2.txt");
+//        nb.DataRead("C:\\Users\\harry\\Downloads\\mush.arff", classidx, 0);
+//        nb.DataRead("C:\\Users\\harry\\Documents\\assignments\\ai\\student-train.arff", classidx, 0);
+        nb.DataRead("C:\\Users\\harry\\Documents\\assignments\\ai\\student-train.arff", 26, 0);
+//        nb.DataRead("C:\\Users\\harry\\Documents\\assignments\\ai\\mush_test.arff", classidx, 1);
+//        nb.buildClassifier(dummy);
+//        int trainSize = (int) Math.round(nb.datas.numInstances() * 0.8);
+//        int testSize = nb.datas.numInstances() - trainSize;
+//        Instances backup = nb.datas;
+//        Instances train = new Instances(nb.datas, 0, trainSize);
+//        Instances test = new Instances(nb.datas, trainSize, testSize);
+//        System.out.println(nb.datas.numInstances());
+//        nb.datas = train;
+//        System.out.println(nb.datas.numInstances());
+        
         nb.buildClassifier(dummy);
+        
+//        nb.save_model();
+//        nb.DataRead("C:\\Users\\harry\\Documents\\assignments\\ai\\mush_test.arff", classidx, 1);
+        
+//        nb.DataRead("C:\\Users\\harry\\Documents\\assignments\\ai\\student-mat-test.arff", classidx, 1);
+        
 //        double x = nb.classifyInstance(nb.datas.instance(0));
+//        Evaluation eval = new Evaluation(nb.datas);
+//        nb.data_test = test;
+//        nb.data_test.setClassIndex(classidx);
+//        nb.Discretize("data_test");
         Evaluation eval = new Evaluation(nb.datas);
-////        eval.evaluateModel(nb, nb.datas);
-////        System.out.println(eval.toSummaryString("=====Sumarry====", true));
         eval.crossValidateModel(nb, nb.datas, 10, new Random(1));
+        
+        eval.evaluateModel(nb, nb.datas);
+////        System.out.println(eval.toSummaryString("=====Sumarry====", true));
+        
         System.out.println(eval.toSummaryString("=====Sumarry=======", true));
         System.out.println(eval.toMatrixString("Confusion matrix : "));
     }
